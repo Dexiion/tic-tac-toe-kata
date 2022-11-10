a4353f0db95841c95f5ea9a606b9e5b6043e614c
Otoniel Pérez <otperez@domingoalonsogroup.com>
2022-11-10 08:18:44 +0000

    Initial commit

---
+++ Tic-Tac-Toe-Kata.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Tic_Tac_Toe_Kata.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
