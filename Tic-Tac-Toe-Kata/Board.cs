ce466e55d2935032d4d4a40dc9b848fa4523d1d4
Otoniel Pérez <otperez@domingoalonsogroup.com>
2022-11-10 09:15:43 +0000

    Refactor: move class Board to its own file

---
+++ Tic-Tac-Toe-Kata/Board.cs
@@ -0,0 +1,28 @@
+﻿namespace Tic;
+
+public class Board
+{
+    private string[,] Matrix;
+
+    public Board()
+    {
+        Matrix = new[,]
+        {
+            {"-","-","-"},
+            {"-","-","-"},
+            {"-","-","-"}
+        };
+    }
+
+    public void SetTileAt(int x, int y, string tile)
+    {
+        if (FieldIsNotEmpty(x, y)) throw new FieldAlreadyTakenException($"Field {x}-{y} is already taken");
+
+        Matrix[x, y] = tile;
+    }
+
+    private bool FieldIsNotEmpty(int x, int y)
+    {
+        return Matrix[x, y] != "-";
+    }
+}
--- Tic-Tac-Toe-Kata/Game.cs
+++ Tic-Tac-Toe-Kata/Game.cs
@@ -28,30 +28,3 @@ public class Game
         return CurrentPlayer.Equals("X") ? "O" :  "X";
     }
 }
-
-public class Board
-{
-    private string[,] Matrix;
-
-    public Board()
-    {
-        Matrix = new[,]
-        {
-            {"-","-","-"},
-            {"-","-","-"},
-            {"-","-","-"}
-        };
-    }
-
-    public void SetTileAt(int x, int y, string tile)
-    {
-        if (FieldIsNotEmpty(x, y)) throw new FieldAlreadyTakenException($"Field {x}-{y} is already taken");
-
-        Matrix[x, y] = tile;
-    }
-
-    private bool FieldIsNotEmpty(int x, int y)
-    {
-        return Matrix[x, y] != "-";
-    }
-}
